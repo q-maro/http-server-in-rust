72a4825b8bb6a01e1325c064e66493c78fb0702a
Roman Maier <rm.roman.maier@gmail.com>
2022-07-27 10:13:25 +0200

    Add dynamic file mapping when calling url paths. Prohibit directory traversal.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,20 +13,32 @@ impl WebsiteHandler {
         Self { public_path }
     }
 
-    fn read_file(&self, file_path:&str) -> Option<String>{
+    fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
     }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        // Response::new(StatusCode::Ok, Some("<h1>Good request</h1>".to_string()))
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             }
             _ => Response::new(StatusCode::NotFound, None),
         }
